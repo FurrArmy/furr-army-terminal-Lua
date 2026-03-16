e0508da3aab93551bff20e4350e3f1fc5f924f8d
FurrArmy <FurrArmy@web.de>
2026-03-16 17:36:47 +0100

    Implement user login and document clearance checks
    Added user authentication and document access control based on clearance levels.

--- furr_terminal.lua
+++ furr_terminal.lua
@@ -1,3 +1,46 @@
+local users = {
+    Mike = {password="Lt.Mike", clearance=10},
+    HR = {password="HR", clearance=3},
+    MR = {password="MR", clearance=2},
+    LR = {password="LR", clearance=1}
+}
+
+local currentUser = nil
+local userClearance = 0
+
+print("Username:")
+local username = read()
+
+print("Password:")
+local password = read("*")
+
+if users[username] and users[username].password == password then
+    currentUser = username
+    userClearance = users[username].clearance
+    print("Login successful")
+    print("Clearance Level: "..userClearance)
+else
+    print("ACCESS DENIED")
+    return
+end
+
+local documents = {
+    facility = {
+        clearance = 1,
+        text = "Furr Army Facility Overview..."
+    },
+
+    weapons = {
+        clearance = 2,
+        text = "Weapon development files..."
+    },
+
+    blackops = {
+        clearance = 10,
+        text = "TOP SECRET: Operation Night Fang"
+    }
+}
+
 function slowPrint(mon, text, speed)
     speed = speed or 0.05
     for i = 1, #text do
@@ -51,6 +94,19 @@ while true do
         monitor.setCursorPos(1,1)
         slowPrint(monitor,"Furr Army Terminal V Alpha 0.6",0.03)
         y = 3
+
+    elseif cmd:sub(1,4) == "doc " then
+    local docName = cmd:sub(5)
+
+    if documents[docName] then
+        if userClearance >= documents[docName].clearance then
+            print(documents[docName].text)
+        else
+            print("ACCESS DENIED - CLEARANCE "..documents[docName].clearance.." REQUIRED")
+        end
+    else
+        print("Document not found")
+    end
 
     else
         monitor.setCursorPos(1,y)
